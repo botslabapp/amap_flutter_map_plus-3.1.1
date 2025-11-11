f71c4d7d547e92e23a6f8c19c9a2ab130c47ee7d
wuxiangting <wuxiangting@360.cn>
2025-11-11 10:04:55 +0800

    amap_flutter_map_plus-3.1.1

---
+++ lib/amap_flutter_map_plus.dart
@@ -0,0 +1,19 @@
+/// 高德地图Flutter插件入口文件
+library amap_flutter_map_plus;
+
+import 'dart:async';
+
+import 'package:amap_flutter_base_plus/amap_flutter_base_plus.dart';
+import 'package:amap_flutter_map_plus/src/core/amap_flutter_platform.dart';
+import 'package:amap_flutter_map_plus/src/core/map_event.dart';
+import 'package:amap_flutter_map_plus/src/core/method_channel_amap_flutter_map.dart';
+import 'package:amap_flutter_map_plus/src/types/types.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+export 'package:amap_flutter_map_plus/src/types/types.dart';
+
+part 'src/amap_controller.dart';
+part 'src/amap_widget.dart';
