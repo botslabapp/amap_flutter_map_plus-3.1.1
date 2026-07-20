86b898e28b4a04df07319e7602aca69124be977f
zjcdxzy <zjcdxzy@hotmail.com>
2026-07-20 15:12:29 +0800

    feat: const_config.dart 增加 ohos key

--- example/lib/const_config.dart
+++ example/lib/const_config.dart
@@ -2,7 +2,7 @@ import 'package:amap_flutter_base_plus/amap_flutter_base_plus.dart';
 
 class ConstConfig {
   static const AMapApiKey amapApiKeys =
-      AMapApiKey(androidKey: '900f72eeee0f21e435cebb0ef155582a', iosKey: '4dfdec97b7bf0b8c13e94777103015a9');
+      AMapApiKey(androidKey: '900f72eeee0f21e435cebb0ef155582a', iosKey: '4dfdec97b7bf0b8c13e94777103015a9',ohosKey: '');  // ohosKey 必须添加
   static const AMapPrivacyStatement amapPrivacyStatement =
       AMapPrivacyStatement(hasContains: true, hasShow: true, hasAgree: true);
 }
